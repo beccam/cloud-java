6ebfd8924c2da7d81a167438eaf9755d6c997761
Rebecca Mills <beccamills86@gmail.com>
2020-04-28 18:39:38 -0230

    change file name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,20 +9,20 @@ public class Main {
    public static void main(String[] args) {
        // Create the CqlSession object:
        try (CqlSession session = CqlSession.builder()
-           .withCloudSecureConnectBundle(Paths.get("/path/to/secure-connect-database_name.zip"))
+           .withCloudSecureConnectBundle(Paths.get("/root/creds.zip"))
            .withAuthCredentials("username","password")
-           .withKeyspace("keyspace_name")
            .build()) {
-           // Select the release_version from the system.local table:
-           ResultSet rs = session.execute("select release_version from system.local");
+           session.execute("CREATE KEYSPACE IF NOT EXISTS user_management WITH replication = {'class': 'NetworkTopologyStrategy', 'caas-dc': '1'}  AND durable_writes = true;");
+           session.execute("CREATE TABLE IF NOT EXISTS users (last_name text, first_name  text, email text, address text,PRIMARY KEY((last_name), first_name, email)");
+           session.execute("INSERT INTO users (last_name, first_name, email, address) VALUES ('Smith', 'Alex','asmith@gmail.com','123 Main st.')");
+           ResultSet rs = session.execute("select first_name, last_name from user_management.users");
            Row row = rs.one();
            //Print the results of the CQL query to the console:
            if (row != null) {
-               System.out.println(row.getString("release_version"));
+               System.out.println(row.getString("first_name"));
            } else {
                System.out.println("An error occurred.");
-           }
-       }
+
        System.exit(0);
    }
 }
