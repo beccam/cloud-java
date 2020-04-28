8d6bc8f01a912bad8a190e3c2c0e93393505d2e9
Rebecca Mills <beccamills86@gmail.com>
2020-04-28 19:09:55 -0230

    change code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,23 +6,31 @@ import java.nio.file.Paths;
 
 public class Main {
 
-   public static void main(String[] args) {
-       // Create the CqlSession object:
-       try (CqlSession session = CqlSession.builder()
-           .withCloudSecureConnectBundle(Paths.get("/root/creds.zip"))
-           .withAuthCredentials("username","password")
-           .build()) {
-           session.execute("CREATE KEYSPACE IF NOT EXISTS user_management WITH replication = {'class': 'NetworkTopologyStrategy', 'caas-dc': '1'}  AND durable_writes = true;");
-           session.execute("CREATE TABLE IF NOT EXISTS users (last_name text, first_name  text, email text, address text,PRIMARY KEY((last_name), first_name, email)");
-           session.execute("INSERT INTO users (last_name, first_name, email, address) VALUES ('Smith', 'Alex','asmith@gmail.com','123 Main st.')");
-           ResultSet rs = session.execute("select first_name, last_name from user_management.users");
-           Row row = rs.one();
-           //Print the results of the CQL query to the console:
-           if (row != null) {
-               System.out.println(row.getString("first_name"));
-           } else {
-               System.out.println("An error occurred.");
+    public static void main(String[] args) {
+        // Create the CqlSession object:
+        try (CqlSession session = CqlSession.builder()
+             .withCloudSecureConnectBundle(Paths.get("/root/creds.zip"))
+             .withAuthCredentials("username","password")
+             .withKeyspace("user_management")
+             .build()) {
+            
+            session.execute(
+                            SimpleStatement.builder( "INSERT INTO users (last_name, first_name, email, address) VALUES (?,?,?,?)")
+                            .addPositionalValues('Smith', 'Alex','asmith@gmail.com','123 Main st.')
+                            .build());
+            
+            
+            ResultSet rs = session.execute(
+                                           SimpleStatement.builder("SELECT * FROM users WHERE last_name=?")
+                                           .addPositionalValue('Smith')
+                                           .build());
+            
+            Row row = rs.one();
+            System.out.format("%s %d\n", row.getString("first_name"), row.getString("email"));
+            
+            System.out.println("An error occurred.");
+      
 
-       System.exit(0);
+         System.exit(0);
    }
 }
