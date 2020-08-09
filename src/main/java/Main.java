29715f8a1c7e7d06cc6e21571cdd92b479df3f5d
Rebecca Mills <beccamills86@gmail.com>
2020-08-09 12:05:54 -0230

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,34 +9,29 @@ public class Main {
     public static void main(String[] args) {
         // Create the CqlSession object:
         try (CqlSession session = CqlSession.builder()
-             .withCloudSecureConnectBundle(Paths.get("/root/creds.zip"))
-             .withAuthCredentials("username","password")
-             .withKeyspace("user_management")
-             .build()) {
-            
-            session.execute("CREATE TABLE IF NOT EXISTS user_management.users (" 
-                  + " last_name text," 
-                  + " first_name text" 
-                  + " email text," 
-                  + " address text," + 
-                    " PRIMARY KEY((last_name), first_name, email)");
-            
+                .withCloudSecureConnectBundle(Paths.get("/root/creds.zip"))
+                .withAuthCredentials("DBUser","DBPassword")
+                .withKeyspace("user_management")
+                .build()) {
+
+            session.execute("CREATE TABLE IF NOT EXISTS user_management.users (last_name text, first_name text, email text, address text, PRIMARY KEY((last_name), first_name, email))");
+
             session.execute(
-                SimpleStatement.builder( "INSERT INTO users (last_name, first_name, email, address) VALUES (?,?,?,?)")
-                .addPositionalValues("Smith", "Alex","asmith@gmail.com","123 Main st.")
-                .build());
-            
-            
+                    SimpleStatement.builder( "INSERT INTO users (last_name, first_name, email, address) VALUES (?,?,?,?)")
+                            .addPositionalValues("Smith", "Alex","asmith@gmail.com","123 Main st.")
+                            .build());
+
+
             ResultSet rs = session.execute(
-               SimpleStatement.builder("SELECT * FROM users WHERE last_name=?")
-               .addPositionalValue("Smith")
-               .build());
-            
+                    SimpleStatement.builder("SELECT * FROM users WHERE last_name=?")
+                            .addPositionalValue("Smith")
+                            .build());
+
             Row row = rs.one();
             System.out.format("%s %s\n", row.getString("first_name"), row.getString("email"));
-        
+
         }
 
-         System.exit(0);
-   }
+        System.exit(0);
+    }
 }
